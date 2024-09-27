175e0a4ac8e851df914c5b10b8d233d8732877f8
JI20 <jonas.itt@tum.de>
2024-09-27 12:05:19 +0200

    Implement simple interactive gradient with WEBGL

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
 import "./App.css";
-import TSLogo from "./assets/ts-logo-standard";
+import { Gradient } from "./components/gradient/Gradient";
 
 function App() {
   return (
-    <div className="bg-purple-light">
-      <TSLogo />
-      <h1>
-        We're glad to have you here! We're currently working hard to finish the
-        Talente Spenden website.
-      </h1>
-      <div>
-        <p>
-          Until then, you can find more information about Talente Spenden at the
-          official website of the TUM Deutschlandstipendium:{" "}
-          <a href="https://www.tum.de/studium/studienfinanzierung/stipendien/stipendien-der-tum/deutschlandstipendium/initiativen">
-            TUM Deutschlandstipendium: Talente Spenden (tum.de)
-          </a>
-        </p>
-        <p>
-          You have an idea and want to talk to us? Feel free to contact us here:{" "}
-          <a href="mailto:talente-spenden@tum.de">talente-spenden@tum.de</a>
-        </p>
-      </div>
+    <div className="left-0 top-0 bg-blue-dark w-screen h-screen">
+      <Gradient />
     </div>
   );
 }

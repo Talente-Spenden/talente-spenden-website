2b869f8799042b639535c743d34ec664925d0abd
JI20 <jonas.itt@tum.de>
2024-09-29 14:15:44 +0200

    Prevent horizontal overflow on desktop and mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import "./App.css";
 import Routes from "./components/router/Routes";
 
 function App() {
-  return <Routes />;
+  return (
+    <div className=" overflow-x-hidden">
+      <Routes />
+    </div>
+  );
 }
 
 export default App;

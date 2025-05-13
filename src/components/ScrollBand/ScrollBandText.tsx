6495dd2abd145fa80d491f4d831c1ed560e0ad59
JI20 <jonas.itt@tum.de>
2025-05-13 08:42:23 +0200

    Add slow down text carousel on hover effect

--- src/components/ScrollBand/ScrollBandText.tsx
+++ src/components/ScrollBand/ScrollBandText.tsx
@@ -1,28 +1,46 @@
 import useMeasure from "react-use-measure";
 import { ScrollCard } from "./ScrollCard";
 import { useMotionValue, animate, motion } from "framer-motion";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const ScrollBandText: React.FC<{
+  // Component adapted from tutorial:"https://www.youtube.com/watch?v=Ot4nZ6UjJLE"
   texts: { type: "Instagram" | "LinkedIn"; text: string; link: string }[];
 }> = ({ texts }) => {
+  const FAST_DURATION = 15;
+  const SLOW_DURATION = 30;
+
+  const [duration, setDuration] = useState(SLOW_DURATION);
   let [ref, { width }] = useMeasure();
   const xTranslation = useMotionValue(0);
+
+  const [mustFinish, setMustFinish] = useState(false);
+  const [rerender, setRerender] = useState(false);
 
   useEffect(() => {
     let controls;
     let finalPosition = -width / 4;
+    if (mustFinish) {
+      controls = animate(xTranslation, [xTranslation.get(), finalPosition], {
+        ease: "linear",
+        duration: duration * (1 - xTranslation.get() / finalPosition),
+        onComplete: () => {
+          setMustFinish(false);
+          setRerender(!rerender);
+        },
+      });
+    } else {
+      controls = animate(xTranslation, [0, finalPosition], {
+        ease: "linear",
+        duration: duration,
+        repeat: Infinity,
+        repeatType: "loop",
+        repeatDelay: 0,
+      });
+    }
 
-    controls = animate(xTranslation, [0, finalPosition], {
-      ease: "linear",
-      duration: 15,
-      repeat: Infinity,
-      repeatType: "loop",
-      repeatDelay: 0,
-    });
-
-    return controls.stop;
-  }, [xTranslation, width]);
+    return controls?.stop;
+  }, [xTranslation, width, duration, rerender]);
 
   return (
     <div className="h-full">
@@ -30,6 +48,14 @@ export const ScrollBandText: React.FC<{
         className="flex w-max h-full"
         ref={ref}
         style={{ x: xTranslation }}
+        onHoverStart={() => {
+          setMustFinish(true);
+          setDuration(SLOW_DURATION);
+        }}
+        onHoverEnd={() => {
+          setMustFinish(true);
+          setDuration(FAST_DURATION);
+        }}
       >
         {[...texts, ...texts, ...texts, ...texts].map((element) => {
           return <ScrollCard element={element} />;
